bc7159e5bd787296dbaa145deb0b1255f2955519
Tobiasz Rolla <rollatobiasz@gmail.com>
2026-05-04 13:14:19 +0200

    feat: ukończono pierwsze zadanie

--- lab7/zadanie1.ps1
+++ lab7/zadanie1.ps1
@@ -1,3 +1,4 @@
+#Dodanie ścierzki jako stałej sprawdzenie i tworzenie folderu jeśli nie istnieje
 [string]$PATH = "./TXTfolder"
 
 if (Test-Path -Path $PATH) {
@@ -6,9 +7,18 @@ if (Test-Path -Path $PATH) {
     Write-Host "folder dose not exist"
     New-Item -Path $PATH -ItemType Directory 
 }
+
+#Inicjalizacja Watchera z filtrem plików txt i ścierzką 
 $watcher = New-Object System.IO.FileSystemWatcher
-$watcher.PATH = "."
-$watcher.Filter = ".txt"
+$watcher.Path = "."
+$watcher.Filter = "*.txt"
 $watcher.EnableRaisingEvents = true
 
-
+# nieskończona pętla czeka aż plik zostanie stworzony jak zostanie przenosi go
+while ($true) {
+    $change = $watcher.WaitForChanged("Created")
+    Write-Host "Wykryto plik: $($change.Name)"
+    
+    Start-Sleep -Milliseconds 500 #krótka przerwa aby plik był załadowany do kopiowania
+    Move-Item -Path $change.Name -Destination $PATH
+}
