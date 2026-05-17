d2683c6536f47e23e11ae21a482b3d624ea14535
Tobiasz Rolla <rollatobiasz@gmail.com>
2026-05-17 14:58:43 +0200

    feat: wykonano pierwszy skrypt z 9 laboratoriów

--- lab9/zadanie1.ps1
+++ lab9/zadanie1.ps1
@@ -1,10 +1,42 @@
+<#
+.SYNOPSIS
+Pobiera aktualny średni kurs waluty do PLN wyświetla 5 ostatnich notowań wraz z datą
+
+.DESCRIPTION
+Skrypt korzysta z API NBP 
+
+.PARAMETER curency_code
+Wymaga podania 3 literowego kodu waluty
+
+.EXAMPLE
+.\zadanie1.ps1 USD
+
+Kurs dolar amerykański
+DATA -> ##### PLN
+DATA -> ##### PLN
+...
+
+.NOTES
+Autor Tobiasz Rolla
+#>
+
+#parametr skryptu kod waluty
 param(
     [Parameter(Mandatory=$true)]
-    [string] $curency_name
+    [string] $currency_code
 )
 
+[string] $NBP_url = "https://api.nbp.pl/api/exchangerates/rates/a/$currency_code/last/5/?format=json"
 
-[string] NBP_url = "https://api.nbp.pl/api/exchangerates/rates/a/$curency_name/today/?format=json"
+$response = Invoke-RestMethod -Uri $NBP_url -Method GET
+
+$currency = $response.currency
+
+Write-Host "Waluta: $currency"
+
+$response.rates | ForEach-Object {
+    Write-Host "$($_.effectiveDate) -> $($_.mid) PLN"
+}
 
 
 
