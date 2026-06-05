b20dcce032eae31b19ee49d0313f573cf970810c
Tobiasz Rolla <rollatobiasz@gmail.com>
2026-06-05 20:08:04 +0200

    feat zad1

--- lab12/SQL_injection.py
+++ lab12/SQL_injection.py
@@ -1,39 +1,11 @@
 import requests
-import sys
-url = sys.argv[1]
 
-payloads = [
-    "'"
-    "''"
-    "')"
-]
-
-sql_errors = [
-    "sql syntax",
-    "mysql",
-    "postgresql",
-    "ora-",
-    "sqlite"
-]
+payloads = ["1", "'", '"']
 
 for payload in payloads:
+    r = requests.get(
+        "http://localhost/DVWA/vulnerabilities/sqli/",
+        params={"id": payload}
+    )
 
-    try:
-        response = requests.get(
-            url,
-            params={"id": payload},
-            timeout=5
-        )
-
-        content = response.text.lower()
-
-        for error in sql_errors:
-            if error in content:
-                print(
-                    f"[!] Możliwa podatność SQL Injection\n"
-                    f"Payload: {payload}\n"
-                    f"Błąd: {error}\n"
-                )
-
-    except Exception as e:
-        print(f"Błąd połączenia: {e}")
+    print(payload, r.status_code)
