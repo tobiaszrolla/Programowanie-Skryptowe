b241230420cbdc4c749da75845e1a429ad7c51d9
Tobiasz Rolla <rollatobiasz@gmail.com>
2026-06-05 20:20:02 +0200

    fix id in url

--- lab12/SQL_injection.py
+++ lab12/SQL_injection.py
@@ -8,7 +8,7 @@ payloads = ["1", "'", '"', "--", "'("]
 for payload in payloads:
     r = requests.get(
         "http://localhost/DVWA/vulnerabilities/sqli/",
-        params={"id": payload}
+        params={"?id": payload}
     )
 
     print(payload, r.status_code)
